1d04100aea00feb62d73740f49293e9cda83ed14
kraisr <27452581+kraisr@users.noreply.github.com>
2023-10-05 16:40:00 -0400

    login form

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -3,8 +3,24 @@ import "../css/login.css";
 import TextField from '@mui/material/TextField';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+import InputLabel from '@mui/material/InputLabel';
+import InputAdornment from '@mui/material/InputAdornment';
+import IconButton from '@mui/material/IconButton';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import FormControl from '@mui/material/FormControl';
 
 function Login() {
+  const [showPassword, setShowPassword] = React.useState(false);
+
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+
   return (
     // <div>
     //   <button onClick={() => window.location.href='/auth/google'}>Login with Google</button>
@@ -20,8 +36,38 @@ function Login() {
           noValidate
           autoComplete="off"
         >
-          <TextField id="filled-basic" label="Username or email" variant="filled" />
-          <TextField id="filled-basic" label="Password" variant="filled" />
+          {/* <TextField id="input-with-sx" label="Username or email" variant="filled" /> */}
+          <TextField
+          id="outlined-username-input"
+          label="Username"
+          type="username"
+          autoComplete="current-username"
+          />
+          <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined">
+            <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+            <OutlinedInput
+              id="outlined-adornment-password"
+              type={showPassword ? 'text' : 'password'}
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    onClick={handleClickShowPassword}
+                    onMouseDown={handleMouseDownPassword}
+                    edge="end"
+                  >
+                    {showPassword ? <VisibilityOff /> : <Visibility />}
+                  </IconButton>
+                </InputAdornment>
+              }
+              label="Password"
+            />
+          </FormControl>
+          {/* <TextField id="filled-basic" label="Password" variant="filled" /> */}
+          {/* <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
+            <AccountCircle sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
+            <TextField id="input-with-sx" label="With sx" variant="standard" />
+          </Box> */}
           <Button variant="contained" style={{backgroundColor: '#fa7070', color: '#fff'}}>Log in</Button>
         </Box>
       </div>
