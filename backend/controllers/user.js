2afe8b4b34e71d3fed5619c7ebafe81a47f20cd9
Ryan Leonard <rleonard772@gmail.com>
2023-10-10 02:08:55 -0400

    i hate my life

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,7 +1,7 @@
 import jwt from 'jsonwebtoken';
 import User from '../models/User.js';
 
-export const user = async (req, res) => {
+export const getUser = async (req, res) => {
     try {
         const token = req.headers.authorization.split(" ")[1];
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
@@ -18,3 +18,48 @@ export const user = async (req, res) => {
         res.status(500).json({ error: err.message });
     }
 }
+
+// Function to update user information
+export const updateUser = async (req, res) => {
+    try {
+        const token = req.headers.authorization.split(" ")[1];
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const userId = decoded.id;
+
+        const { 
+            firstName,
+            username,
+            bio,
+            link,
+            image, 
+        } = req.body;
+    
+        const updatedUserData = {
+            firstName,
+            username,
+            bio,
+            link
+        };
+
+        const updatedUser = await User.findByIdAndUpdate(
+            userId,
+            { 
+                firstName: firstName,
+                username: username,
+                bio: bio,
+                link: link,
+                image: image,
+              },
+            { new: true }
+        ).select('-password');
+
+        if (!updatedUser) {
+            return res.status(400).json({ error: "User does not exist" });
+        }
+
+        res.status(200).json({ message: 'Profile updated successfully', user: updatedUser });
+
+        } catch (error) {
+            res.status(500).json({ error: error.message });
+        }
+  }
