e6f2aec404a8042205e8ddd1e20398bd8601a28a
michael01890 <limichael640@gmail.com>
2023-10-11 17:31:55 -0400

    Wrote post request to post ingredient to pantry

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -18,3 +18,55 @@ export const user = async (req, res) => {
         res.status(500).json({ error: err.message });
     }
 }
+
+export const addIngredient = async (req, res) => {
+    try {
+        const token = req.headers.authorization.split(" ")[1];
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const userId = decoded.id;
+
+        // Log the decoded user ID
+        console.log("Decoded User ID:", userId);
+
+        const ingredient = req.body;
+        console.log("Ingredient:", ingredient);
+
+
+        const updatedPantry = await User.findByIdAndUpdate(
+            userId,
+            { $push: { pantry: ingredient } },
+            { new: true }
+        ).select('-password');
+
+        // Log the updated pantry
+        console.log("Updated Pantry:", updatedPantry);
+
+        if (!updatedPantry) {
+            return res.status(400).json({ error: "Error updating pantry for user" });
+        }
+
+        res.status(200).json(updatedPantry);
+    } catch (err) {
+        console.error("Error in addIngredient function:", err); // Log the error for more details
+        res.status(500).json({ error: err.message });
+    }
+};
+
+
+export const getFromPantry = async (req, res) => {
+    try {
+        const token = req.headers.authorization.split(" ")[1];
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const userId = decoded.id;
+
+        const user = await User.findById(userId);
+
+        if (!user) {
+            return res.status(400).json({ error: "User does not exist" });
+        }
+
+        res.status(200).json(user.pantry);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+}
