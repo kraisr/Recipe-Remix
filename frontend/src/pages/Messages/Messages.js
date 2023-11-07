f6e278cb331cefd936c35177623ed219d9f490c2
michael01890 <limichael640@gmail.com>
2023-11-07 15:14:44 -0500

    Created Messages page

---
+++ frontend/src/pages/Messages/Messages.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react';
+import './messages.css';
+
+
+const Messages = () => {
+
+    return(
+        <div>Messages</div>
+    );
+
+}
+
+export default Messages;
