3feb9f7537372762ac0a45af8896c9029125b019
kraisr <27452581+kraisr@users.noreply.github.com>
2023-11-09 21:58:35 -0500

    rewrite post schema and fetching for community

--- backend/models/Post.js
+++ backend/models/Post.js
@@ -12,15 +12,34 @@ const postSchema = mongoose.Schema({
   name: String, 
   image: String,
   caption: String,
-  isCustom: Boolean,
-  customRecipe: {
-    image: String,
-    name: String,
-    ingredients: [String],
-  },
+  ingredients: [{
+    type: String,
+    required: false,
+  }],
+
   createdAt: {
     type: Date,
-    default: new Date()
+    default: Date.now,
+  },
+  ratings: [
+    {
+      type: Number,
+      min: 0,
+      max: 5,
+    },
+  ],
+}, {
+  toJSON: { virtuals: true },
+  toObject: { virtuals: true },
+});
+
+// Virtual field for average rating
+postSchema.virtual('averageRating').get(function() {
+  if (this.ratings.length > 0) {
+    const sum = this.ratings.reduce((acc, rating) => acc + rating, 0);
+    return parseFloat((sum / this.ratings.length).toFixed(2)); // Rounds to 2 decimal places
+  } else {
+    return 0; // Default to 0 if no ratings
   }
 });
 
--- frontend/src/components/StarRating/StarRating.js
+++ frontend/src/components/StarRating/StarRating.js
@@ -1,22 +1,46 @@
 import React, { useState } from 'react';
 import './starRating.css';
 
-const StarRating = () => {
+const StarRating = ({ postId }) => {
   const [currentRating, setCurrentRating] = useState(0);
   const [hoverRating, setHoverRating] = useState(undefined);
 
+  const submitRating = async (ratingValue) => {
+    const payload = { postId, rating: ratingValue }; // Prepare payload with postId and rating
+
+    try {
+      const response = await fetch('http://localhost:8080/posts/addRating', { // Endpoint to handle rating updates
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          // Include authorization headers if needed
+        },
+        body: JSON.stringify(payload),
+      });
+      console.log('postId: ', postId);
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      const responseData = await response.json();
+      console.log('Rating submitted:', responseData);
+      // You can set state or trigger a re-fetch of post data here if needed
+    } catch (error) {
+      console.error('Error submitting rating:', error);
+    }
+  };
 
   const handleStarClick = (ratingValue) => {
     setCurrentRating(ratingValue);
+    submitRating(ratingValue);
   };
 
   const handleStarHover = (ratingValue) => {
     setHoverRating(ratingValue);
   };
 
-
   const handleStarHoverOut = () => {
-    setHoverRating(undefined); // Reset hover rating
+    setHoverRating(undefined);
   };
 
   return (
@@ -25,9 +49,9 @@ const StarRating = () => {
         const ratingValue = index + 1;
         let style = {};
         if (hoverRating !== undefined) {
-            style = ratingValue <= hoverRating ? { color: 'orange' } : {};
+          style = ratingValue <= hoverRating ? { color: 'orange' } : {};
         } else {
-            style = ratingValue <= currentRating ? { color: 'orange' } : {};
+          style = ratingValue <= currentRating ? { color: 'orange' } : {};
         }
 
         return (
